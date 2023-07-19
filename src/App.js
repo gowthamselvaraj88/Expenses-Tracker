7db498e7d49bc7f2afb7abcb8cacdcebdf326de7
gowthamselvarj88 <gowthams87225@gmail.com>
2023-07-19 09:45:30 +0530

    deploy fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -10,59 +10,17 @@ import TextField from '@mui/material/TextField';
 // import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 // import Input from '@mui/material/Input';
 // import FilledInput from '@mui/material/FilledInput';
-import OutlinedInput from '@mui/material/OutlinedInput';
+
 import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-// import FormHelperText from '@mui/material/FormHelperText';
-import FormControl from '@mui/material/FormControl';
-// import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import { FixedSizeList } from 'react-window';
 import AppBar from '@mui/material/AppBar';
 import Stack from '@mui/material/Stack';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-// import moment from 'moment
 
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 
-
-function renderRow() {
- 
-  return (
-    <ListItem  component="div" disablePadding>
-      <ListItemButton>
-        <ListItemText primary={"Transaction"} />
-      </ListItemButton>
-    </ListItem>
-  );
-}
-
-function appBarLabel(label) {
-  return (
-    <Toolbar>
-      {/* <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-        <MenuIcon />
-      </IconButton> */}
-      <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
-        {label}
-      </Typography>
-    </Toolbar>
-  );
-}
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#1976d2',
-    },
-  },
-});
 
 //Get current date and time
 
@@ -74,138 +32,153 @@ const formatDate = (date) => {
   const minutes = String(date.getMinutes()).padStart(2, '0');
   const seconds = String(date.getSeconds()).padStart(2, '0');
 
-  return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 };
-
-
 
 const Budget = () => {
 
   const initialTransaction = [
-    { Acion: "",
-      Datatime: "",
-     Balance: (0)}
-   ];
-   const date = new Date(); // Assuming you want to format the current date and time
-   const formattedDate = formatDate(date);
-   const [values, setValues] = useState('');
-   const [balance, setBalance] = useState(0);
-   const [Transaction,setTransaction] = useState(initialTransaction)
-   // "Datetime": ' ',
- 
-   const handleChange = (event) => {
-     setValues(event.target.value);
-   };
- 
-   const handleSubmit = (event) => {
-     event.preventDefault();
-     setBalance(parseInt(values));
-     setValues('');
-   };
- 
-   const handleAdd = (event) => {
-    event.preventDefault();
-     setBalance((prevBalance) => prevBalance + parseInt(values));
-    //  setTransaction((prevTransaction) => [
-    //    ...prevTransaction,
-    //    {"action": "Add", "Balance Amount":{balance}}
-    //  ]);
-     setValues('');
-    //  moment(date).format('YYYY-MM-DD')
-     console.log("Transaction",Transaction)
-     console.log("date",formattedDate);
-   };
+    {
+      Acion: "",
+      dateTime: "",
+      Balance: (0)
+    }
+  ];
+  const date = new Date(); // Assuming you want to format the current date and time
+  const formattedDate = formatDate(date);
+  // const [values, setValues] = useState(0);
+  const [inputValue, setInputValue] = useState();
+  const [balance, setBalance] = useState(0);
+  const [transaction, setTransaction] = useState([]);
 
-   useEffect(() => {
-    setTransaction((prevTransaction) => [
-      ...prevTransaction,
-      {"action": "Add","Datatime":{formattedDate}, "Balance":{balance}}
-    ]);
-    console.log("Transaction", Transaction);
-  }, [balance]);
-  
-   // "Datatime":Date(),
- 
-   const handleRemove = (event) => {
-    event.preventDefault();
-     setBalance((prevBalance) => prevBalance - parseInt(values));
-     setValues('');
-     console.log("Transaction",Transaction)
-   };
-   useEffect(() => {
-    setTransaction((prevTransaction) => [
-      ...prevTransaction,
-      {"action": "Remove","Datatime":{formattedDate}, "Balance":{balance}}
-    ]);
-    console.log("Transaction", Transaction);
-  }, [balance]);
- 
+  useEffect(() => {
+    console.log('inputValue:', inputValue);
+  }, [inputValue]);
 
-  return (    
-  
-<div>
-    <Stack spacing={2} sx={{ flexGrow: 1 }}>
-      <ThemeProvider theme={darkTheme}>
-        <AppBar position="relative" color="primary">
-          {appBarLabel('Expence Tracker')}
-        </AppBar>
-      </ThemeProvider>
-    </Stack>
+  const handleChange = (event) => {
+    setInputValue(parseInt(event.target.value));
+  }
 
 
 
+  const handleAdd = (event) => {
 
-<div style={{ display: "flex", justifyContent: "center" }}>
+    setBalance((prevBalance) => prevBalance + parseInt(event));
 
-      {/* First Card */}
-      <Card sx={{ minWidth: 400, mr: 2 }}>
-        <CardContent>
-           {/* <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-        </FormControl> */}
-        <FormControl onSubmit={handleSubmit} fullWidth sx={{ m: 1 }}>
-          <InputLabel htmlFor="outlined-adornment-amount">Balance</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-amount"
-            startAdornment={<InputAdornment position="start">$ {balance}</InputAdornment>}
-            label="Amount"
-          />
-          {/* <Button size="small" type='submit' variant="contained" color="success">ADD</Button>   */}
-        </FormControl>
-        <TextField id="filled-basic" onChange={handleChange} label="Enter the Amount" variant="filled">
-          </TextField>
-        </CardContent>
-        <CardActions>
-          <Button size="small" type='submit' onClick={handleAdd} variant="contained" color="success">ADD</Button>                                   
-          <Button size="small" onClick={handleRemove} variant="outlined" color="error" >Remove</Button>
-        </CardActions>
-      </Card>
 
-      {/* Second Card */}
-      <Card sx={{ minWidth: 400, ml: 2 }}>
-        <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            <b>Transaction value:</b>
-            <Box
-      sx={{ width: '100%', height: 400, maxWidth: 360, bgcolor: 'background.paper' }}
-    >
-      <FixedSizeList
-        height={400}
-        width={360}
-        itemSize={46}
-        itemCount={10}
-        overscanCount={5}
-      >
-        {renderRow}
-      </FixedSizeList>
-    </Box>
-          
-          </Typography>
-        </CardContent>
-      </Card>
+  }
+
+
+  const handleRemove = (event) => {
+    setBalance((prevBalance) => prevBalance - parseInt(event));
+
+  }
+
+
+  const handleAction = (actionType) => {
+    let value = inputValue.toString();
+    console.log("value::", value)
+    const regex = /^[0-9\b]+$/;
+    if (value.trim() !== '' && /^\d+$/.test(value)) {
+      let date = formattedDate;
+      console.log("date = ", date)
+      console.log("date = ", regex);
+      console.log("action type :: ", actionType, "value ::", value);
+
+      setTransaction((prevtransaction) => [
+        ...prevtransaction,
+        { "action": actionType, "dateTime": date, "value": value }
+      ]);
+
+      if (actionType === "ADD") {
+        handleAdd(value);
+      } else {
+        handleRemove(value);
+      }
+      setInputValue("");
+      console.log("input value =", inputValue)
+    
+    }
+    else {
+      alert(("Enter only Numbers"));
+      setInputValue("");
+
+    }
+
+    console.log("value:: end", inputValue)
+  }
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#1976d2',
+      },
+    },
+  })
+  function appBarLabel(label) {
+    return (
+      <Toolbar>
+        {/* <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <MenuIcon />
+        </IconButton> */}
+        <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+          {label}
+        </Typography>
+      </Toolbar>
+    );
+  }
+
+
+
+  return (
+
+    <div>
+      <Stack spacing={2} sx={{ flexGrow: 1 }}>
+        <ThemeProvider theme={darkTheme}>
+          <AppBar position="relative" color="primary">
+            {appBarLabel('Expence Tracker')}
+          </AppBar>
+        </ThemeProvider>
+      </Stack>
+
+      <div style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
+        {/* First Card */}
+        <Card sx={{ minWidth: 400, mr: 2 }}>
+          <CardContent>
+            <InputLabel style={{ fontWeight: 500, color: "black", marginBottom: "20px" }} htmlFor="outlined-adornment-amount">Balance: $ <span>{balance}</span></InputLabel>
+            <TextField id="filled-basic" type='number' value={inputValue} onChange={handleChange} label="Enter the Amount" variant="filled">
+            </TextField>
+
+          </CardContent>
+          <CardActions>
+            <Button size="small" onClick={() => handleAction("ADD")} variant="contained" color="success">ADD</Button>
+            <Button size="small" onClick={() => handleAction("REMOVE")} variant="outlined" color="error" >Remove</Button>
+          </CardActions>
+        </Card>
+
+        {/* Second Card */}
+        <Card sx={{ minWidth: 400, ml: 2 }}>
+          <CardContent>
+            <InputLabel style={{ fontWeight: 500, color: "black" }} htmlFor="outlined-adornment-amount">Transaction Details</InputLabel>
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+              {transaction.map((data) => (
+                <ListItem
+                  key={data.dateTime}
+
+                >
+                  <ListItemText style={{
+                    color: data.action === 'ADD' ? 'green' : 'red', fontWeight:400,
+                  }} primary={`Action: ${data.action}`} secondary={`Value: ${data.value}, Date: ${data.dateTime}`} />
+                </ListItem>
+              ))}
+            </List>
+          </CardContent>
+        </Card>
+      </div>
     </div>
-    </div>
-  );
+  )
 }
+
 
 export default Budget;
 
--- src/index.js
+++ src/index.js
@@ -8,12 +8,11 @@ import { StyledEngineProvider } from '@mui/material/styles';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     
-     <StyledEngineProvider injectFirst>
-     <App />
-     
+
+    <StyledEngineProvider injectFirst>
+      <App />
     </StyledEngineProvider>
-     
+
   </React.StrictMode>
 );
 
